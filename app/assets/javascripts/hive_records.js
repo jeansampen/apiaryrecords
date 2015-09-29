f6a41893da3887ed644e06b9b7ff4f018100a90b
jeansampen <jeansampen93@gmail.com>
2015-09-29 17:35:16 -0400

    hide optional fields on HiveRecord form III

--- app/assets/javascripts/hive_records.js
+++ app/assets/javascripts/hive_records.js
@@ -12,4 +12,16 @@ $(document).ready(function() {
     $(this).nextAll().slice(0,2).toggleClass('disease_optional_record_field');
   });
 
+  $('#hive_record_medication').click(function() {
+    $(this).nextAll().slice(0,4).toggleClass('med_optional_record_field')
+  });
+
+  $('#hive_record_ipm_used').click(function() {
+    $(this).nextAll().slice(0,1).toggleClass('ipm_optional_record_field')
+  });
+
+  $('#hive_record_replace_equipment').click(function() {
+    $(this).nextAll().slice(0,3).toggleClass('replace_equip_optional_record_field')
+  });
+
 });
