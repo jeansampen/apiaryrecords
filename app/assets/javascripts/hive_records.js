92e011c89cba53a9b14357e9d55b8b265e3381cb
jeansampen <jeansampen93@gmail.com>
2015-09-29 17:26:12 -0400

    hide optional fields on HiveRecord form II

--- app/assets/javascripts/hive_records.js
+++ app/assets/javascripts/hive_records.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  
+  $('#hive_record_queen_cells').click(function() {
+    $(this).nextAll().slice(0,4).toggleClass('queen_optional_record_field')
+  });
+
+  $('#hive_record_disease_or_pest').click(function() {
+    console.log("click");
+    $(this).nextAll().slice(0,2).toggleClass('disease_optional_record_field');
+  });
+
+});
